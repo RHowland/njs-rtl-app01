853b06a64a7a31eb717a6f9f69b248efff50351f
Son-OfAnton <admasterefe00@gmail.com>
2024-06-25 18:25:59 +0300

    feat: Done with setting up testing

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import Home from '@/app/page';
 
-it('should have Discover text', () => {
+it('should have Learn more text', () => {
 	render(<Home />);
-	const headerText = expect(screen.getByText('Discover'));
+	const headerText = expect(screen.getByText('Learn more'));
 	headerText.toBeInTheDocument();
 });
