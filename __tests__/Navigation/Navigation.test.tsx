ef060ed51f02a70b6aac94489f8442a57803a840
Son-OfAnton <admasterefe00@gmail.com>
2024-06-30 18:33:01 +0300

    feat: Refactored navigation tests

--- __tests__/Navigation/Navigation.test.tsx
+++ __tests__/Navigation/Navigation.test.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import Navbar from '@/components/Navbar';
-import { fireEvent, render, screen } from '@testing-library/react';
 import Footer from '@/components/Footer';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-describe('Navbar', () => {
-	beforeEach(() => {
-		render(<Navbar />);
-	});
-
-	it('should have a logo', () => {
-		const logo = screen.getByRole('img');
-		expect(logo).toBeInTheDocument();
-	});
-
+const testCommonLinks = () => {
 	it('should have an about us link', () => {
 		const aboutUs = screen.getByRole('link', { name: /about us/i });
 		expect(aboutUs).toBeInTheDocument();
@@ -46,6 +37,19 @@ describe('Navbar', () => {
 		fireEvent.click(contactLink);
 		expect(contactLink.closest('a')).toHaveAttribute('href', '/ContactPage');
 	});
+};
+
+describe('Navbar', () => {
+	beforeEach(() => {
+		render(<Navbar />);
+	});
+
+	it('should have a logo', () => {
+		const logo = screen.getByRole('img');
+		expect(logo).toBeInTheDocument();
+	});
+
+	testCommonLinks();
 });
 
 describe('Footer', () => {
@@ -58,36 +62,5 @@ describe('Footer', () => {
 		expect(logo).toHaveLength(5);
 	});
 
-	it('should have an about us link', () => {
-		const aboutUs = screen.getByRole('link', { name: /about us/i });
-		expect(aboutUs).toBeInTheDocument();
-	});
-
-	it('should have a services link', () => {
-		const services = screen.getByRole('link', { name: /services/i });
-		expect(services).toBeInTheDocument();
-	});
-
-	it('should have a contact link', () => {
-		const contact = screen.getByRole('link', { name: /contact/i });
-		expect(contact).toBeInTheDocument();
-	});
-
-	it('navigates to the About Us page when the About Us link is clicked', () => {
-		const aboutLink = screen.getByRole('link', { name: /about us/i });
-		fireEvent.click(aboutLink);
-		expect(aboutLink.closest('a')).toHaveAttribute('href', '/AboutPage');
-	});
-
-	it('navigates to the Services page when the Services link is clicked', () => {
-		const servicesLink = screen.getByRole('link', { name: /services/i });
-		fireEvent.click(servicesLink);
-		expect(servicesLink.closest('a')).toHaveAttribute('href', '/ServicesPage');
-	});
-
-	it('navigates to the Contact page when the Contact link is clicked', () => {
-		const contactLink = screen.getByRole('link', { name: /contact/i });
-		fireEvent.click(contactLink);
-		expect(contactLink.closest('a')).toHaveAttribute('href', '/ContactPage');
-	});
+	testCommonLinks();
 });
